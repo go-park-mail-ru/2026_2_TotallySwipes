28ff27fc7bb6037e5c75e840e04ed1645a6d5a7f
Alexander <alex.quesss@gmail.com>
2026-09-26 12:44:35 +0300

    fix: brought the interfaces to the service level

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -13,20 +13,9 @@ var (
 	ErrNotFound = fmt.Errorf("not found")
 )
 
-type UserRepository interface {
-	CreateUser(ctx context.Context, input *model.UserInput) error
-	GetUserByEmail(ctx context.Context, email string) (*model.User, error)
-	GetUserByID(ctx context.Context, id int64) (*model.User, error)
-
-	UpdateUser(ctx context.Context, id int64, input *model.UpdateUserInput) (*model.User, error)
-	DeleteUserByID(ctx context.Context, id int64) error
-}
-
 type UserRepo struct {
 	db *sql.DB
 }
-
-var _ UserRepository = (*UserRepo)(nil)
 
 func NewUserRepository(db *sql.DB) *UserRepo {
 	return &UserRepo{db: db}
