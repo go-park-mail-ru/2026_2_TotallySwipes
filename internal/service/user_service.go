455d2a4635ab7ddf3643d1838e64b77fd0c5fd1b
Alexander <alex.quesss@gmail.com>
2026-09-26 17:09:10 +0300

    add user service

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -13,3 +13,41 @@ type UserRepository interface {
 	UpdateUser(ctx context.Context, id int64, input *model.UpdateUserInput) (*model.User, error)
 	DeleteUserByID(ctx context.Context, id int64) error
 }
+
+type UserService interface {
+	CreateUser(ctx context.Context, input *model.UserInput) error
+
+	GetUserByEmail(ctx context.Context, email string) (*model.User, error)
+	GetUserByID(ctx context.Context, id int64) (*model.User, error)
+
+	UpdateUser(ctx context.Context, id int64, input *model.UpdateUserInput) (*model.User, error)
+	DeleteUserByID(ctx context.Context, id int64) error
+}
+
+type UserServiceImpl struct {
+	userRepo UserRepository
+}
+
+func NewUserService(repo UserRepository) *UserServiceImpl {
+	return &UserServiceImpl{userRepo: repo}
+}
+
+func (s *UserServiceImpl) CreateUser(ctx context.Context, input *model.UserInput) error {
+	return s.userRepo.CreateUser(ctx, input)
+}
+
+func (s *UserServiceImpl) GetUserByEmail(ctx context.Context, email string) (*model.User, error) {
+	return s.userRepo.GetUserByEmail(ctx, email)
+}
+
+func (s *UserServiceImpl) GetUserByID(ctx context.Context, id int64) (*model.User, error) {
+	return s.userRepo.GetUserByID(ctx, id)
+}
+
+func (s *UserServiceImpl) UpdateUser(ctx context.Context, id int64, input *model.UpdateUserInput) (*model.User, error) {
+	return s.userRepo.UpdateUser(ctx, id, input)
+}
+
+func (s *UserServiceImpl) DeleteUserByID(ctx context.Context, id int64) error {
+	return s.userRepo.DeleteUserByID(ctx, id)
+}
